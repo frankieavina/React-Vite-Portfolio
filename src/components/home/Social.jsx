ecb2817972dbea4a36e3610f9d04b3d3bab94f75
francisco avina <frankieavina@Frankies-MacBook-Pro.local>
2024-07-22 15:25:59 -0700

    ft(home-com):Finalized Home Screen/Components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        <a href="https://www.instagram.com" className="home_social-icon" target='_blank'>
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.dribble.com" className="home_social-icon" target='_blank'>
+            <i className="uil uil-dribbble"></i>
+        </a>
+
+        <a href="https://wwww.github.com" className="home_social-icon" target='_blank'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
