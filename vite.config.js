65418c20064a43dbf15baea6187990a36a59e77e
francisco avina <frankieavina@Frankies-MacBook-Pro.local>
2024-08-23 15:41:39 -0700

    fa(add):Deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/React-Vite-Portfolio"
+  base:"/React-Vite-Portfolio/"
 })
